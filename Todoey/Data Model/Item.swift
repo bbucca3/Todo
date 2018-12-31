15f05d6dc9ac132302347b5c24ad2cbd623caf36
Ben Bucca <benjamin.bucca3@gmail.com>
2018-12-31 18:04:56 -0500

    Adds folders

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Benjamin Bucca on 12/31/18.
+//  Copyright © 2018 Benjamin Bucca. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
